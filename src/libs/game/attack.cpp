modawan/reone

commit decd8dfd49168c3aeb6963bdcf1d6086cd5c41ff

[game] Fix ranged attack functions for droids
Droids differ significantly from regular humanoid creatures:

Although they have weapons to calculate attacks, and they do not have
dedicated models for them, and they are not attached to rhand/lhand
model attachments. Therefore projectiles take rbullet/lbullet hooks as
origin points for projectiles.

Animations for droids are also different: there are b0a* animations
instead of standard animations based on a wield type (b5a1, for
example). It seems that there is no way to check if a creature is
supposed to use a standard animation or b0a* animation. Some droids
are humanoid-like and use regular weapons. Therefore the patch tries
both, and uses whatever animation is present.

src/libs/game/attack.cpp
@@ -246,29 +246,50 @@ AttackResultType AttackBuffer::result() const {
     return sortedByScore[bestIndex];
 }
 
+std::shared_ptr<Item> determineProjectileWeapon(Creature &attacker, Projectile::Source source) {
+    int slot = (source == Projectile::Main)
+                   ? InventorySlots::rightWeapon
+                   : InventorySlots::leftWeapon;
+
+    std::shared_ptr<Item> weapon(attacker.getEquippedItem(slot));
+    if (!weapon) {
+        slot = (source == Projectile::Main)
+                   ? InventorySlots::leftWeapon
+                   : InventorySlots::rightWeapon;
+
+        weapon = attacker.getEquippedItem(slot);
+    }
+
+    return weapon;
+}
+
+static glm::vec3 determineProjectileOrigin(scene::ModelSceneNode &model, Projectile::Source source) {
+    std::string attachment = (source == Projectile::Main) ? "rhand" : "lhand";
+    auto weaponModel = static_cast<scene::ModelSceneNode *>(model.getAttachment(attachment));
+    if (weaponModel) {
+        auto bulletHook = weaponModel->getNodeByName("bullethook");
+        if (bulletHook) {
+            return bulletHook->origin();
+        }
+        weaponModel->origin();
+    }
+
+    // Droids do not have weapon model, but they have hooks in the main (body) model.
+    std::string directAttachment = (source == Projectile::Main) ? "rbullet" : "lbullet";
+    if (scene::SceneNode *direct = model.getNodeByName(directAttachment)) {
+        return direct->origin();
+    }
+
+    return model.origin();
+}
+
 void Projectile::fire(Creature &attacker, Object &target, scene::ISceneGraph &sceneGraph) {
     auto attackerModel = std::static_pointer_cast<scene::ModelSceneNode>(attacker.sceneNode());
     auto targetModel = std::static_pointer_cast<scene::ModelSceneNode>(target.sceneNode());
     if (!attackerModel || !targetModel)
         return;
 
-    int slot = 0;
-    const char *attachment = nullptr;
-
-    switch (_source) {
-    case Projectile::Main: {
-        slot = InventorySlots::rightWeapon;
-        attachment = "rhand";
-        break;
-    }
-    case Projectile::Offhand:
-        slot = InventorySlots::leftWeapon;
-        attachment = "lhand";
-        break;
-    }
-    assert(slot && attachment && "unhandled projectile source");
-
-    std::shared_ptr<Item> weapon(attacker.getEquippedItem(slot));
+    std::shared_ptr<Item> weapon = determineProjectileWeapon(attacker, _source);
     if (!weapon)
         return;
 
@@ -276,18 +297,7 @@ void Projectile::fire(Creature &attacker, Object &target, scene::ISceneGraph &sc
     if (!ammunitionType)
         return;
 
-    auto weaponModel = static_cast<scene::ModelSceneNode *>(attackerModel->getAttachment(attachment));
-    if (!weaponModel)
-        return;
-
-    // Determine projectile position
-    glm::vec3 projectilePos;
-    auto bulletHook = weaponModel->getNodeByName("bullethook");
-    if (bulletHook) {
-        projectilePos = bulletHook->origin();
-    } else {
-        projectilePos = weaponModel->origin();
-    }
+    glm::vec3 projectilePos = determineProjectileOrigin(*attackerModel, _source);
 
     // Determine projectile direction
     auto impact = targetModel->getNodeByName("impact");
@@ -459,6 +469,30 @@ bool navigateToAttackTarget(Creature &attacker, Object &target, float dt, bool &
 
     reachedOnce = true;
     return true;
+}
+
+static bool hasAnim(const graphics::Model &model, const std::string &anim) {
+    return model.animations().count(anim);
+}
+
+std::string getRangedAttackAnim(Creature &attacker, int kind) {
+    CreatureWieldType wield = attacker.getWieldType();
+    assert(isRangedWieldType(wield) && "invalid wield");
+
+    auto attackerModel = std::static_pointer_cast<scene::ModelSceneNode>(attacker.sceneNode());
+    const graphics::Model &model = attackerModel->model();
+
+    std::string animByWield = str(boost::format("b%da%d") % static_cast<int>(wield) % kind);
+    if (hasAnim(model, animByWield)) {
+        return animByWield;
+    }
+
+    std::string animBasic = str(boost::format("b0a%d") % kind);
+    if (hasAnim(model, animBasic)) {
+        return animBasic;
+    }
+
+    return "";
 }
 
 } // namespace game

include/reone/game/attack.h
@@ -225,6 +225,8 @@ private:
 
 bool navigateToAttackTarget(Creature &attacker, Object &actor, float dt, bool &reachedOnce);
 
+std::string getRangedAttackAnim(Creature &attacker, int kind);
+
 } // namespace game
 
 } // namespace reone

src/libs/game/action/attackobject.cpp
@@ -68,10 +68,6 @@ static std::string getMeleeParryAnim(CreatureWieldType targetWield, int variant)
     return str(boost::format("g%dr1") % static_cast<int>(targetWield));
 }
 
-static std::string getRangedAttackAnim(CreatureWieldType attackerWield) {
-    return str(boost::format("b%da1") % static_cast<int>(attackerWield));
-}
-
 static std::string getRangedDamageAnim(CreatureWieldType targetWield) {
     return str(boost::format("g%dd1") % static_cast<int>(targetWield));
 }
@@ -108,7 +104,7 @@ static void attack(const CombatRound &round, Creature &attacker, Object &target,
 
     std::string attackAnim = isMelee
                                  ? getMeleeAttackAnim(attackerWield, targetWield, variant, round.duel)
-                                 : getRangedAttackAnim(attackerWield);
+                                 : getRangedAttackAnim(attacker, /*kind=*/1);
 
     attacker.playAnimation(attackAnim, animProp);
 

commit b1c55449993d693bd6027d8b4d580edffea14109

[game] Fix animations for ranged attacks
hasAnim should look into super models recursively to traverse a
complete list of animations.

The patch fixes a regression introduced in #58.

src/libs/game/attack.cpp
@@ -472,7 +472,15 @@ bool navigateToAttackTarget(Creature &attacker, Object &target, float dt, bool &
 }
 
 static bool hasAnim(const graphics::Model &model, const std::string &anim) {
-    return model.animations().count(anim);
+    if (model.animations().count(anim)) {
+        return true;
+    }
+
+    if (std::shared_ptr<graphics::Model> super = model.superModel()) {
+        return hasAnim(*super, anim);
+    }
+
+    return false;
 }
 
 std::string getRangedAttackAnim(Creature &attacker, int kind) {