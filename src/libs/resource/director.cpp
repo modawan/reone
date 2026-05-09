modawan/reone

commit 6f3319451e645d56a1ab4344db5ece4c1e5c6267

[resource] Support nested ERF containers
This change adds support for savegame.sav ERF files that contain:

 - GFF resources (utc, faction, inventory)
 - ERF modules

Once a savegame.sav is loaded, these nested ERF modules are available
as resources (byte buffers), but we need to add them as containers.

The patch abstracts the underlying storage of ERF and RIM files to be
either a file, or a memory buffer. Resources service is extended to
add them as such.

The patch also introduces loadRIM and loadERF functions instead of
direct filesystem lookups. These functions first attempt to find a
container nested in currently loaded containers, and then fallback to
file system lookups. This allows us to resolve to savegame modules
instead of regular game modules.

src/libs/resource/director.cpp
@@ -171,41 +171,55 @@ void ResourceDirector::loadGlobalResources() {
 }
 
 void ResourceDirector::loadModuleResources(const std::string &name) {
-    auto modulesPath = findFileIgnoreCase(_gamePath, kModulesDirectoryName);
+    std::optional<std::filesystem::path> modulesPath = findFileIgnoreCase(_gamePath, kModulesDirectoryName);
     if (!modulesPath) {
         throw ResourceNotFoundException("Modules directory not found");
     }
 
-    auto &resources = _resources;
-    auto rimPath = findFileIgnoreCase(*modulesPath, name + ".rim");
-    if (rimPath) {
-        resources.addRIM(*rimPath, ContainerKind::Local);
-    }
-    auto rimsPath = findFileIgnoreCase(*modulesPath, name + "_s.rim");
-    if (rimsPath) {
-        resources.addRIM(*rimsPath, ContainerKind::Local);
-    }
-    auto modPath = findFileIgnoreCase(*modulesPath, name + ".mod");
-    if (modPath) {
-        resources.addERF(*modPath, ContainerKind::Local);
-    }
-    if (!rimPath && !rimsPath && !modPath) {
-        throw ResourceNotFoundException("Module archives not found: " + name);
-    }
-
-    auto lipsPath = findFileIgnoreCase(_gamePath, kLipsDirectoryName);
-    if (lipsPath) {
-        auto locModPath = findFileIgnoreCase(*lipsPath, name + "_loc.mod");
-        if (locModPath) {
-            resources.addERF(*locModPath, ContainerKind::Local);
-        }
-    }
+    loadRIM(*modulesPath, name, ContainerKind::Local);
+    loadRIM(*modulesPath, name + "_s", ContainerKind::Local);
+    loadERF(*modulesPath, name, ContainerKind::Local);
+    loadERF(*modulesPath, name + "_loc", ContainerKind::Local);
 
     if (_gameId == GameID::TSL) {
-        auto dlgErfPath = findFileIgnoreCase(*modulesPath, name + "_dlg.erf");
-        if (dlgErfPath) {
-            resources.addERF(*dlgErfPath, ContainerKind::Local);
-        }
+        loadERF(*modulesPath, name + "_dlg", ContainerKind::Local);
+    }
+}
+
+void ResourceDirector::loadRIM(const std::filesystem::path &path, const std::string &name, ContainerKind kind) {
+    // Try to find a module with the same name in already loaded resources.
+    // Same idea as in loadERF.
+    std::optional<Resource> res = _resources.find(ResourceId(name, ResType::Res));
+    if (res) {
+        _resources.addMemRIM(res->data, kind);
+        return;
+    }
+
+    if (auto rimPath = findFileIgnoreCase(path, name + ".rim")) {
+        _resources.addRIM(*rimPath, kind);
+    }
+}
+
+void ResourceDirector::loadERF(const std::filesystem::path &path, const std::string &name, ContainerKind kind) {
+    // Try to find a module with the same name in already loaded resources.
+    //
+    // This allows us to support savegame archives: savegame.sav is an ERF
+    // archive that contains ERF modules. When loading from a save game we add
+    // savegame.sav ERF container. Module lookups resolve to modules from this
+    // container, and fall back to filesystem search if the module is not in the
+    // save archive.
+    std::optional<Resource> res = _resources.find(ResourceId(name, ResType::Mod));
+    if (res) {
+        _resources.addMemERF(res->data, kind);
+        return;
+    }
+
+    if (auto modPath = findFileIgnoreCase(path, name + ".mod")) {
+        _resources.addERF(*modPath, kind);
+    }
+
+    if (auto erfPath = findFileIgnoreCase(path, name + ".erf")) {
+        _resources.addERF(*erfPath, kind);
     }
 }
 

include/reone/resource/container/erf.h
@@ -17,9 +17,8 @@
 
 #pragma once
 
-#include "reone/system/stream/fileinput.h"
-
 #include "../container.h"
+#include "utils.h"
 
 namespace reone {
 
@@ -27,9 +26,8 @@ namespace resource {
 
 class ErfResourceContainer : public IResourceContainer, boost::noncopyable {
 public:
-    ErfResourceContainer(std::filesystem::path path) :
-        _path(std::move(path)) {
-    }
+    ErfResourceContainer(Storage storage) :
+        _storage(std::move(storage)) {}
 
     void init();
 
@@ -48,8 +46,7 @@ private:
         uint32_t fileSize {0};
     };
 
-    std::filesystem::path _path;
-    std::unique_ptr<FileInputStream> _erf;
+    Storage _storage;
 
     std::unordered_set<ResourceId> _resourceIds;
     std::unordered_map<ResourceId, Resource> _idToResource;

include/reone/resource/container/utils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 The reone project contributors
+ * Copyright (c) 2026 The reone project contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,42 +18,33 @@
 #pragma once
 
 #include "reone/system/stream/fileinput.h"
-
-#include "../container.h"
-#include "utils.h"
+#include "reone/system/stream/memoryinput.h"
 
 namespace reone {
 
 namespace resource {
 
-class RimResourceContainer : public IResourceContainer, boost::noncopyable {
+class Storage {
 public:
-    RimResourceContainer(Storage storage) :
-        _storage(std::move(storage)) {}
+    Storage(std::filesystem::path path) :
+        _path(path), _file(std::make_unique<FileInputStream>(_path)) {}
+    Storage(ByteBuffer buffer) :
+        _buffer(buffer), _mem(std::make_unique<MemoryInputStream>(_buffer)) {}
 
-    void init();
-
-    // IResourceContainer
-
-    std::optional<ByteBuffer> findResourceData(const ResourceId &id) override;
-
-    const std::unordered_set<ResourceId> &resourceIds() const override { return _resourceIds; }
-
-    // END IResourceContainer
+    IInputStream &stream() {
+        assert((_file || _mem) && "uninitialized storage");
+        if (_file) {
+            return *_file;
+        }
+        return *_mem;
+    }
 
 private:
-    struct Resource {
-        ResourceId id;
-        uint32_t offset {0};
-        uint32_t fileSize {0};
-    };
-
-    Storage _storage;
-
-    std::unordered_set<ResourceId> _resourceIds;
-    std::unordered_map<ResourceId, Resource> _idToResource;
+    std::filesystem::path _path;
+    std::unique_ptr<FileInputStream> _file;
+    ByteBuffer _buffer;
+    std::unique_ptr<MemoryInputStream> _mem;
 };
 
 } // namespace resource
-
 } // namespace reone

include/reone/resource/director.h
@@ -17,6 +17,7 @@
 
 #pragma once
 
+#include "resources.h"
 #include "types.h"
 
 namespace reone {
@@ -99,6 +100,8 @@ private:
 
     void loadGlobalResources();
     void loadModuleResources(const std::string &name);
+    void loadRIM(const std::filesystem::path &path, const std::string &name, ContainerKind kind);
+    void loadERF(const std::filesystem::path &path, const std::string &name, ContainerKind kind);
 };
 
 } // namespace resource

include/reone/resource/resources.h
@@ -51,7 +51,9 @@ public:
     virtual void addEXE(const std::filesystem::path &path) = 0;
     virtual void addKEY(const std::filesystem::path &path) = 0;
     virtual void addERF(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) = 0;
+    virtual void addMemERF(ByteBuffer buffer, ContainerKind kind) = 0;
     virtual void addRIM(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) = 0;
+    virtual void addMemRIM(ByteBuffer buffer, ContainerKind kind = ContainerKind::Global) = 0;
     virtual void addFolder(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) = 0;
 
     virtual Resource get(const ResourceId &id) = 0;
@@ -86,7 +88,9 @@ public:
     void addEXE(const std::filesystem::path &path) override;
     void addKEY(const std::filesystem::path &path) override;
     void addERF(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) override;
+    void addMemERF(ByteBuffer buffer, ContainerKind kind) override;
     void addRIM(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) override;
+    void addMemRIM(ByteBuffer buffer, ContainerKind kind = ContainerKind::Global) override;
     void addFolder(const std::filesystem::path &path, ContainerKind kind = ContainerKind::Global) override;
 
     Resource get(const ResourceId &id) override;

src/libs/resource/container/erf.cpp
@@ -24,9 +24,7 @@ namespace reone {
 namespace resource {
 
 void ErfResourceContainer::init() {
-    _erf = std::make_unique<FileInputStream>(_path);
-
-    auto reader = ErfReader(*_erf);
+    auto reader = ErfReader(_storage.stream());
     reader.load();
 
     auto &keys = reader.keys();
@@ -54,8 +52,9 @@ std::optional<ByteBuffer> ErfResourceContainer::findResourceData(const ResourceI
     ByteBuffer buf;
     buf.resize(resource.fileSize);
 
-    _erf->seek(resource.offset, SeekOrigin::Begin);
-    _erf->read(&buf[0], buf.size());
+    IInputStream &erf = _storage.stream();
+    erf.seek(resource.offset, SeekOrigin::Begin);
+    erf.read(&buf[0], buf.size());
 
     return buf;
 }

src/libs/resource/container/rim.cpp
@@ -25,9 +25,7 @@ namespace reone {
 namespace resource {
 
 void RimResourceContainer::init() {
-    _rim = std::make_unique<FileInputStream>(_path);
-
-    auto reader = RimReader(*_rim);
+    auto reader = RimReader(_storage.stream());
     reader.load();
 
     for (auto &rimResource : reader.resources()) {
@@ -52,8 +50,9 @@ std::optional<ByteBuffer> RimResourceContainer::findResourceData(const ResourceI
     ByteBuffer buf;
     buf.resize(resource.fileSize);
 
-    _rim->seek(resource.offset, SeekOrigin::Begin);
-    _rim->read(&buf[0], buf.size());
+    IInputStream &rim = _storage.stream();
+    rim.seek(resource.offset, SeekOrigin::Begin);
+    rim.read(&buf[0], buf.size());
 
     return buf;
 }

src/libs/resource/resources.cpp
@@ -40,8 +40,20 @@ void Resources::addERF(const std::filesystem::path &path, ContainerKind kind) {
     _containers.push_front(ResourceContainerPair {std::move(provider), kind});
 }
 
+void Resources::addMemERF(ByteBuffer buffer, ContainerKind kind) {
+    auto provider = std::make_unique<ErfResourceContainer>(std::move(buffer));
+    provider->init();
+    _containers.push_front(ResourceContainerPair {std::move(provider), kind});
+}
+
 void Resources::addRIM(const std::filesystem::path &path, ContainerKind kind) {
     auto provider = std::make_unique<RimResourceContainer>(path);
+    provider->init();
+    _containers.push_front(ResourceContainerPair {std::move(provider), kind});
+}
+
+void Resources::addMemRIM(ByteBuffer buffer, ContainerKind kind) {
+    auto provider = std::make_unique<RimResourceContainer>(buffer);
     provider->init();
     _containers.push_front(ResourceContainerPair {std::move(provider), kind});
 }

commit 102917b6b31d6c48fa7e253087376e101d20487f

[resource] Load lips ERF for a module
Fixed a regression from 6f3319451 "[resource] Support nested ERF
containers". Lips movement in dialogs was broken because we did not
load these extra ERF modules.

src/libs/resource/director.cpp
@@ -199,6 +199,10 @@ void ResourceDirector::loadModuleResources(const std::string &name) {
     loadERF(*modulesPath, name, ContainerKind::Local);
     loadERF(*modulesPath, name + "_loc", ContainerKind::Local);
 
+    if (auto lipsPath = findFileIgnoreCase(_gamePath, kLipsDirectoryName)) {
+        loadERF(*lipsPath, name + "_loc", ContainerKind::Local);
+    }
+
     if (_gameId == GameID::TSL) {
         loadERF(*modulesPath, name + "_dlg", ContainerKind::Local);
     }