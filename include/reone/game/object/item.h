f022633aa53bb2975a2ca7384d19e425737b4b7d
modawan <modawan42@proton.me>
2026-08-13 17:34:05 +0900

    [game] Implement GiveItem routine
    GiveItem routine is supposed to move an item from the current owner to
    another object. The routine itself only has an item and a destination
    object, so the engine has to track the current owner for every item.
    
    GiveItem moves an entire stack of items to avoid splitting it, which
    would otherwise require to clone the item.
    
    This fixes Leviathan setup, where LEV_StripCharacter script loops over
    all items in the target's equipment and inventory and moves them to a
    container.

--- include/reone/game/object/item.h
+++ include/reone/game/object/item.h
@@ -137,6 +137,9 @@ public:
     void setIdentified(bool value);
     void setEquipped(bool equipped);
 
+    uint32_t owner() const { return _ownerId; }
+    void setOwner(uint32_t id) { _ownerId = id; }
+
 private:
     // Serializable
     int32_t _baseItem {0};
@@ -154,6 +157,8 @@ private:
     uint8_t _textureVariation {0};
     bool _dropable {false};
     // END Serializable
+
+    uint32_t _ownerId {0};
 
     std::string _baseBodyVariation;
     std::string _itemClass;
