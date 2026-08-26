7f8927c53909706783802d7a789436e57cc9fec7
Eldbury <leneldbury@gmail.com>
2026-08-26 19:51:19 +1000

    [test] Use standard GPL header

--- test/game/loadfromsavegame.cpp
+++ test/game/loadfromsavegame.cpp
@@ -1,4 +1,11 @@
-/* Copyright (c) 2026 The reone project contributors */
+/*
+ * Copyright (c) 2026 The reone project contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
